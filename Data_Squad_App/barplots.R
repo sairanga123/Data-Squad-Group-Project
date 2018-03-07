c058a906a8fe6e6ff61c795a080d80f0e0885c67
Srinithi Sellakumaran Latha <slsri@uw.edu>
2018-03-06 16:26:26 -0800

    add barplots

--- Data_Squad_App/barplots.R
+++ Data_Squad_App/barplots.R
@@ -16,3 +16,13 @@ net_neutrality_without_retweets <- barplot(c(nrow(net_neutrality_support_wo_retw
 immigration_ban_without_retweets <- barplot(c(nrow(immigration_ban_support_wo_retweets), nrow(immigration_ban_against_wo_retweets)), main="Position on Immigration Ban without Retweets", 
                                             xlab = "Position", ylab = "# of Tweets", names.arg = c("Support", "Against"), horiz = FALSE, col = c("grey", "grey"))
 
+
+# grouped bar plots
+data_gun_control_support <- c(nrow(San_Francisco_gun_control_df_support), nrow(Seattle_gun_control_df_support), 
+                              nrow(Washington_gun_control_df_support), nrow(Oklahoma_gun_control_df_support), nrow(Omaha_gun_control_df_support))
+data_gun_control_against <- c(nrow(San_Francisco_gun_control_df_against), nrow(Seattle_gun_control_df_against), 
+                              nrow(Washington_gun_control_df_against), nrow(Oklahoma_gun_control_df_against), nrow(Omaha_gun_control_df_against))
+gun_control_data <- rbind(data_gun_control_support, data_gun_control_against)
+gun_control_grouped <- barplot(gun_control_data, beside = TRUE, names.arg = c("San Francisco", "Seattle", "Washington", "Oklahoma City", "Omaha"), 
+                               main = "Gun Control Position in 5 Cities", xlab = "Position", ylab = "# of Tweets", col = c("green", "orange"))
+legend("topright", legend = c("Support", "Against"), fill = c("green", "orange"))
