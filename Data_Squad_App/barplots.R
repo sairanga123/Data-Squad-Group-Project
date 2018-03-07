3ce23a4a8b1e0ca832eabb8b04e187fa261eac7c
Maddi Cummins <maddic9@uw.edu>
2018-03-07 04:03:20 -0800

    changed "Washington" to "Washington DC"

--- Data_Squad_App/barplots.R
+++ Data_Squad_App/barplots.R
@@ -23,7 +23,7 @@ citiesbarplot <- function(policy){
     data_gun_control_against <- c(nrow(San_Francisco_gun_control_df_against), nrow(Seattle_gun_control_df_against), 
                                   nrow(Washington_gun_control_df_against), nrow(Oklahoma_gun_control_df_against), nrow(Omaha_gun_control_df_against))
     gun_control_data <- rbind(data_gun_control_support, data_gun_control_against)
-    gun_control_grouped <- barplot(gun_control_data, beside = TRUE, names.arg = c("San Francisco", "Seattle", "Washington", "Oklahoma City", "Omaha"), 
+    gun_control_grouped <- barplot(gun_control_data, beside = TRUE, names.arg = c("San Francisco", "Seattle", "Washington DC", "Oklahoma City", "Omaha"), 
                                    main = "Gun Control Position in 5 Cities", xlab = "Position", ylab = "# of Tweets", col = c("green", "orange"))
     legend("topright", legend = c("Support", "Against"), fill = c("green", "orange"))
   }else if(policy == "net_neutrality"){  
@@ -32,7 +32,7 @@ citiesbarplot <- function(policy){
     net_neutrality_control_against <- c(nrow(San_Francisco_net_neutrality_df_against), nrow(Seattle_net_neutrality_df_against), 
                                   nrow(Washington_net_neutrality_df_against), nrow(Oklahoma_net_neutrality_df_against), nrow(Omaha_net_neutrality_df_against))
     net_neutrality_data <- rbind(net_neutrality_control_support, net_neutrality_control_against)
-    net_neutrality_grouped <- barplot(net_neutrality_data, beside = TRUE, names.arg = c("San Francisco", "Seattle", "Washington", "Oklahoma City", "Omaha"), 
+    net_neutrality_grouped <- barplot(net_neutrality_data, beside = TRUE, names.arg = c("San Francisco", "Seattle", "Washington DC", "Oklahoma City", "Omaha"), 
                                    main = "Net Neutrality Position in 5 Cities", xlab = "Position", ylab = "# of Tweets", col = c("green", "orange"))
     legend("topright", legend = c("Support", "Against"), fill = c("green", "orange"))
   }else if(policy == "immigration"){  
@@ -41,7 +41,7 @@ citiesbarplot <- function(policy){
     immigration_ban_control_against <- c(nrow(San_Francisco_immigration_ban_df_against), nrow(Seattle_immigration_ban_df_against), 
                                         nrow(Washington_immigration_ban_df_against), nrow(Oklahoma_immigration_ban_df_against), nrow(Omaha_immigration_ban_df_against))
     immigration_ban_data <- rbind(immigration_ban_control_support, immigration_ban_control_against)
-    immigration_ban_grouped <- barplot(immigration_ban_data, beside = TRUE, names.arg = c("San Francisco", "Seattle", "Washington", "Oklahoma City", "Omaha"), 
+    immigration_ban_grouped <- barplot(immigration_ban_data, beside = TRUE, names.arg = c("San Francisco", "Seattle", "Washington DC", "Oklahoma City", "Omaha"), 
                                       main = "Immigration Ban Position in 5 Cities", xlab = "Position", ylab = "# of Tweets", col = c("green", "orange"))
     legend("topright", legend = c("Support", "Against"), fill = c("green", "orange"))
   }
