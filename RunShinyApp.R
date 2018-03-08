79fa6d6d2a189b379bdd85191a7b230d1d496465
Yujun Tang <tyj0401@uw.edu>
2018-03-07 23:12:45 -0800

    uploaded to Shiny Server

--- RunShinyApp.R
+++ RunShinyApp.R
@@ -1,8 +1,8 @@
 library(shiny)
 library(dplyr)
 #source("APIs.R") #if you want to re-pull the data uncomment this line and comment out the next one
-load("~/Data-Squad-Group-Project/APIEnvironment.RData")
-source("~/Data-Squad-Group-Project/barplots.R")
-source("~/Data-Squad-Group-Project/analysis.R")
-source("~/Data-Squad-Group-Project/timegraphfunction.R")
-runApp("~/Data-Squad-Group-Project/Data_Squad_App")
+load("APIEnvironment.RData")
+source("barplots.R")
+source("analysis.R")
+source("timegraphfunction.R")
+runApp("Data_Squad_App")
